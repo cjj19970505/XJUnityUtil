af50e4511fb7a16837cf9cf055402ef8ddedfe37
陈俊嘉 <cjj19970505@live.cn>
2019-01-27 01:13:27 +0900

    AddUnityProject

---
+++ XJUnityUtilUnityProj/Assets/AutoRotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    public float Angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.rotation = Quaternion.Euler(0, Angle, 0);
+
+    }
+
+    void jsTestFunc(float angle)
+    {
+        Angle = angle;
+    }
+}
---
+++ XJUnityUtilUnityProj/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XJUnityUtil.Unity;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public XJUnityUtilCenter XJUnityUtilCenter;
+    public List<string> Messages;
+    // Start is called before the first frame update
+    void Start()
+    {
+        XJUnityUtilCenter.CommToApplication.Received += _OnEditorCommToAppReceived;
+    }
+
+    private void _OnEditorCommToAppReceived(object sender, string e)
+    {
+        Messages.Add(e);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
